bc06a757f1dcce3877da078443e20d43bf089c1b
Aubin <aubin.mahe@wanadoo.fr>
2022-02-04 16:11:47 +0100

    Simplification de l'interface, traitement de la clef de Fa

--- public/lecture-de-notes.js
+++ public/lecture-de-notes.js
@@ -1,43 +1,45 @@
+const NOTES = ["Do", "Ré", "Mi", "Fa", "Sol", "La", "Si"];
+
 class LectureDeNotes {
    
-   #count          = document.getElementById('note-count');
-   #interval       = document.getElementById('time-interval');
-   #with_names     = document.getElementById('with-names');
-   #round          = document.getElementById('round');
-   #start          = document.getElementById('start');
-   #portées        = document.getElementById('portées');
-   #clavier        = document.getElementById('clavier');
-   #note           = document.getElementById('note');
-   #diese          = document.getElementById('diese');
-   #timer_id       = -1;
-   #previous_index = -1;
+   #avec_l_aide                = document.getElementById('avec-l-aide');
+   #démarrer                   = document.getElementById('démarrer');
+   #portées                    = document.getElementById('portées');
+   #clavier                    = document.getElementById('clavier');
+   #note                       = document.getElementById('note');
+   #diese                      = document.getElementById('diese');
+   #nbr_de_notes_decodées      = document.getElementById('nbr-de-note-decodée');
+   #nbr_d_erreurs              = document.getElementById('nombre-d-erreurs');
+   #temps_de_lecture           = document.getElementById('temps-de-lecture');
+   #correction_block           = document.getElementById('correction-bloc');
+   #correction                 = document.getElementById('correction');
+   #valider_la_correction      = document.getElementById('valider');
+   #somme_des_temps_de_lecture = 0;
+   #clef_de_sol                = true;
+   #index                      = -1;
+   #alteration                 = false;
+   #previous_index             = -1;
+   #instant_de_la_question     = 0;
 
    constructor() {
-      this.#note.style.left = ( this.#portées.offsetLeft + 200 ) + "px";
-      this.#note.style.top  = ( this.#portées.offsetTop + 4 - ( 4 - 6 ) * 6.5 ) + "px";
-      this.#clavier.addEventListener('click', event => this.#show_note( event ));
-      this.#with_names.addEventListener('click', () => this.#toggle_display_note_names());
-      this.#start.addEventListener('click', () => {
-         if( this.#timer_id == -1 ) {
-            let interval = parseInt( this.#interval.value );
-            console.log( "starting test with interval = " + interval );
-            this.#timer_id = setInterval(() => this.#display_note(), interval );
-            this.#display_note();
-         }
-         else {
-            console.log( "interrupting test" );
-            clearInterval( this.#timer_id );
-            this.#timer_id           = -1;
-            this.#round.value        =  1;
-            this.#note.style.display = 'none';
-         }
+      this.#clavier.addEventListener('click', event => this.#note_selected( event ));
+      this.#avec_l_aide.addEventListener('click', () => this.#toggle_display_note_names());
+      this.#démarrer.addEventListener('click', () => {
+         this.#nbr_de_notes_decodées.value = "0";
+         this.#nbr_d_erreurs        .value = "0";
+         this.#temps_de_lecture     .value = "0";
+         this.#display_note();
       });
+      this.#valider_la_correction.addEventListener('click', () => this.#display_note());
    }
-   
-   #show_note( event ) {
+
+   #note_selected( event ) {
+      let nbr_de_notes_decodées = parseInt( this.#nbr_de_notes_decodées.value ) + 1;
+      this.#somme_des_temps_de_lecture += Date.now() - this.#instant_de_la_question;
+      this.#nbr_de_notes_decodées.value = nbr_de_notes_decodées;
+      this.#temps_de_lecture     .value = "" + ( this.#somme_des_temps_de_lecture / nbr_de_notes_decodées / 1000 ).toFixed(1);
       let x = event.clientX - this.#clavier.offsetLeft;
       let y = event.clientY - this.#clavier.offsetTop;
-      console.log( "x, y: " + x + ", " + y );
       let index      = Math.floor( x / 75 );
       let alteration = false;
       if( y < 200 ) {
@@ -67,52 +69,66 @@ class LectureDeNotes {
             alteration = true;
          }
       }
-      console.log( "index: " + index );
-      this.#note.style.left    = ( this.#portées.offsetLeft + 200 ) + "px";
-      this.#note.style.top     = ( this.#portées.offsetTop + 4 - ( index - 6 ) * 6.5 ) + "px";
+      if( ! this.#clef_de_sol ) {
+         this.#index += 2;
+      }
+      this.#index %= 7;
+      if(( this.#index == index )&&( this.#alteration == alteration)) {
+         this.#display_note();
+      }
+      else {
+         this.#nbr_d_erreurs.value = parseInt( this.#nbr_d_erreurs.value ) + 1;
+         this.#correction_block.style.visibility = 'visible';
+      }
+   }
+
+   #display_note() {
+      this.#note .style.left = ( this.#portées.offsetLeft + 200 ) + "px";
+      this.#diese.style.left = ( this.#portées.offsetLeft + 186 ) + "px";
+      this.#correction_block.style.visibility = 'hidden';
+      this.#clef_de_sol = ( Math.random() > 0.5 );
+      this.#index       = this.#previous_index;
+      while( this.#index == this.#previous_index ) {
+         this.#index = Math.round( Math.random() * 14.0 );
+      }
+      if( this.#index > 13 ) {
+         this.#index = 13;
+      }
+      this.#previous_index = this.#index;
+      this.#alteration     = ( Math.random() > 0.5 );
+      if( this.#clef_de_sol ) {
+         this.#note.style.top = ( this.#portées.offsetTop +  4 - ( this.#index - 6 ) * 6.5 ) + "px";
+         if(( this.#index == 2 )||( this.#index == 6 )||( this.#index == 9 )||( this.#index == 13 )) {
+            this.#alteration = false;
+         }
+      }
+      else {
+         this.#note.style.top = ( this.#portées.offsetTop + 70 - ( this.#index - 11 ) * 6.5 ) + "px";
+         if(( this.#index == 0 )||( this.#index == 4 )||( this.#index == 7 )||( this.#index == 11 )) {
+            this.#alteration = false;
+         }
+      }
+      if( this.#clef_de_sol ) {
+         this.#diese.style.top = ( this.#portées.offsetTop +  34 - ( this.#index - 6 ) * 6.5 ) + "px";
+         this.#correction.value = NOTES[this.#index % 7] + ( this.#alteration ? "#" : "" );
+      }
+      else {
+         this.#diese.style.top = ( this.#portées.offsetTop + 100 - ( this.#index - 11 ) * 6.5 ) + "px";
+         this.#correction.value = NOTES[( this.#index + 2 ) % 7] + ( this.#alteration ? "#" : "" );
+      }
+      console.log(this.#correction.value);
       this.#note.style.display = 'block';
-      console.log( "note.top: " + this.#note.style.top );
-      if( alteration ) {
+      if( this.#alteration ) {
          this.#diese.style.display = 'block';
       }
       else {
          this.#diese.style.display = 'none';
       }
-   }
-
-   #display_note() {
-      let clef  = Math.round( Math.random() *  2.0 );
-      if( clef > 1 ) {
-         clef = 1;
-      }
-      let index = this.#previous_index;
-      while( index == this.#previous_index ) {
-         index = Math.round( Math.random() * 14.0 );
-      }
-      if( index > 13 ) {
-         index = 13;
-      }
-      this.#previous_index = index;
-      console.log( "index: " + index );
-      this.#note.style.left    = ( this.#portées.offsetLeft + 200 ) + "px";
-      this.#note.style.top     = ( this.#portées.offsetTop + 4 - ( index - 6 ) * 6.5 ) + "px";
-      this.#note.style.display = 'block';
-      let round = parseInt( this.#round.value );
-      let count = parseInt( this.#count.value );
-      if( ++round > count ) {
-         console.log( "ending test" );
-         clearInterval( this.#timer_id );
-         this.#timer_id           = -1;
-         this.#round.value        =  1;
-         this.#note.style.display = 'none';
-      }
-      else {
-         this.#round.value = round;
-      }
+      this.#instant_de_la_question = Date.now();
    }
    
    #toggle_display_note_names() {
-      if( this.#with_names.checked ) {
+      if( this.#avec_l_aide.checked ) {
          this.#clavier.src = 'clavier.jpg';
       }
       else {
